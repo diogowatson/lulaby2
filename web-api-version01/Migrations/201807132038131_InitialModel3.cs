ad10a523808eca28d30dc5b66da20b8ab6599d75
Diogo Andrade <diogowatson@gmail.com>
2018-07-16 10:39:24 -0400

    finished for now

---
+++ web-api-version01/Migrations/201807132038131_InitialModel3.cs
@@ -0,0 +1,16 @@
+namespace web_api_version01.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialModel3 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
